cae3702d82642fa2b0a7fd0405701656b21033c2
KateEllen <katemaher00@gmail.com>
2022-01-23 19:06:40 +0000

    Create index and base templates

--- blog/admin.py
+++ blog/admin.py
@@ -1,1 +1,21 @@
-from django.contrib import adminfrom .models import Post
+from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    
+    list_filter = ('status', 'created_on')
+    search_fileds = ['title', 'content']
+    summernote_fields = ('content')
+    
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fileds = ('name', 'email', 'body')
+    actions = ['approve_comments']
+    
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
--- simpage/urls.py
+++ simpage/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog.views import home_page
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page, name='home_page')
+    path('summernote/', include('django_summernote.urls')),
+    path('', include('blog.urls'), name ='blog_urls'),
+   
 ]

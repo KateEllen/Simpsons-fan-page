5875d54f9d64d6f7f7af300160db6a05162d9844
KateEllen <katemaher00@gmail.com>
2022-02-07 19:45:48 +0000

    Add comment section

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from django.views.generic import ListView, DetailView
 from .models import Post
@@ -16,6 +16,7 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'blog_details.html'
     
+    
 class PostDetail(View):
 
     def get(self, request, slug, *args, **kwargs):
@@ -28,11 +29,41 @@ class PostDetail(View):
 
         return render(
             request,
-            "post_detail.html",
+            "blog_details.html",
             {
                 "post": post,
                 "comments": comments,
                 "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+    def post(self, request, slug, *args, **kwargs):
+        
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+        
+        return render(
+            request,
+            "blog_details.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
                 "liked": liked,
                 "comment_form": CommentForm()
             },

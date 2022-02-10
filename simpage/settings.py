7d8945a8127835e085d2f686d183cdd9cc86f658
KateEllen <katemaher00@gmail.com>
2022-02-10 18:33:29 +0000

    Install whitenoise

--- simpage/settings.py
+++ simpage/settings.py
@@ -32,6 +32,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['simpsons-blog.herokuapp.com', 'localhost']
 
+STATICFILES_DIRS
 
 # Application definition
 
@@ -62,6 +63,7 @@ LOGOUT_REDIRECT_URL = '/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -143,7 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 

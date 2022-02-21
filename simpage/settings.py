235a35544dbad0b04a9378ce1a2f8a19bcf18851
KateEllen <katemaher00@gmail.com>
2022-02-21 07:15:53 +0000

    remove STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' from settings.py

--- simpage/settings.py
+++ simpage/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 

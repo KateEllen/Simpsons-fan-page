3b82e8ea4542e44f608efcc8445a2c94dee34aa8
KateEllen <katemaher00@gmail.com>
2022-06-03 23:23:54 +0000

    Refactor edit character url

--- blog/urls.py
+++ blog/urls.py
@@ -1,15 +1,16 @@
 from . import views
 from django.urls import path
-from .views import HomeView, BlogDetailView, AddCommentView, CharacterDetail
 
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
+    path('', views.HomeView.as_view(), name='home'),
     path('blog/<int:pk>', views.BlogDetailView.as_view(), name='blog_detail'),
-    path('blog/<int:pk>/comment/', AddCommentView.as_view(), name='add_comment'),
+    path('blog/<int:pk>/comment/',
+         views.AddCommentView.as_view(), name='add_comment'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     path('characters/', views.CharacterList.as_view(), name='character_list'),
-    path('character/<int:pk>/', views.CharacterDetail.as_view(), name='character_detail'),
+    path('character/<int:pk>/', views.CharacterDetail.as_view(),
+         name='character_detail'),
     path('<pk>/update_character/',
          views.EditCharacterView.as_view(),
          name='update_character'
@@ -19,11 +20,13 @@ urlpatterns = [
         views.DeleteCharacterView.as_view(),
         name='delete_character'
     ),
-    path('characters/add/', views.AddCharactersView.as_view(), name="character_add"),
-    
-     path('editcomment/<int:comment_id>/',
-         views.edit_comment, name='edit_comment'),
+    path('characters/add/', views.AddCharactersView.as_view(), 
+         name="character_add"
+         ),
 
-    path('deletecomment/<int:comment_id>/',
-         views.delete_comment, name='delete_comment')
+    # path('edit_comment/<int:comment_id>/',
+    #      views.edit_comment, name='edit_comment'),
+
+    # path('delete_comment/<int:comment_id>/',
+    #      views.delete_comment, name='delete_comment')
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,6 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
 
-
 class HomeView(ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
@@ -74,7 +73,9 @@ class EditCharacterView(UpdateView):
     model = Characters
     form_class = CharacterEditForm
     template_name = 'edit_character.html'
-    success_url = reverse_lazy("characters")
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy("character_detail", args=[str(self.object.pk)])
 
 
 @method_decorator(login_required(
@@ -104,35 +105,49 @@ class PostLike(View):
             post.likes.add(request.user)
 
         return HttpResponseRedirect(reverse('blog_detail', args=[slug]))
-    
-@login_required
-def edit_comment(request, comment_id):
-    """ Edit Comment """
-    """ Prefill Form """
-    comment = get_object_or_404(Comment, pk=comment_id)
-    if request.user == comment.comment_author or request.user.is_superuser:
-        if request.method == 'POST':
-            comment_form = CommentForm(request.POST, instance=comment)
-            if comment_form.is_valid():
-                comment_form.save()
-                messages.success(request, 'Comment successfully updated')
-                return redirect(reverse('view_explore'))
-            else:
-                messages.error(request,
-                               'Error - Please check form is valid and \
-                                    try again.')
-        else:
-            comment_form = CommentForm(instance=comment)
-            messages.info(request, f'You are editing {comment.comment_title}')
-    else:
-        messages.error(request, 'Sorry, only the comment author can do that')
-        return redirect(reverse('home'))
 
-    template = 'explore/edit_comment.html'
-    context = {
-        'comment_form': comment_form,
-        'comment': comment,
-        'on_profile_page': True
-    }
+# @login_required
 
-    return render(request, template, context)
+# class EditComment(UpdateView):
+#     def edit_comment(self, request, comment_id):
+#         """ Edit Comment """
+#         """ Prefill Form """
+#         comment = get_object_or_404(Comment, pk=comment_id)
+#         if request.user == comment.comment_author or request.user.is_superuser:
+#             if request.method == 'POST':
+#                 comment_form = CommentForm(request.POST, instance=comment)
+#                 if comment_form.is_valid():
+#                     comment_form.save()
+#                     messages.success(request, 'Comment successfully updated')
+#                     return redirect(reverse('blog_detail', args=[comment.post]))
+#                 else:
+#                     messages.error(request,
+#                                 'Error - Please check form is valid and \
+#                                         try again.')
+#             else:
+#                 comment_form = CommentForm(instance=comment)
+#                 messages.info(request, f'You are editing {comment.comment_title}')
+#         else:
+#             messages.error(request, 'Sorry, only the comment author can do that')
+#             return redirect(reverse('home'))
+
+#         template = 'explore/edit_comment.html'
+#         context = {
+#             'comment_form': comment_form,
+#             'comment': comment,
+#             'on_profile_page': True
+#         }
+
+#         return render(request, template, context)
+
+# @login_required
+# def delete_comment(request, comment_id):
+#     comment = get_object_or_404(Comment, pk=comment_id)
+#     if request.user == comment.comment_author or request.user.is_superuser:
+#         """ Delete Comment """
+#         comment.delete()
+#         messages.success(request, 'Comment deleted')
+#         return redirect(reverse('view_explore'))
+#     else:
+#         messages.error(request, 'Sorry, only the comment author can do that')
+#         return redirect(reverse('home'))
